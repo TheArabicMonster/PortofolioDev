d3b788f1b4eeeada0b1cf17cbecd345d76eb9d43
TheArabicMonster <neetamk@icloud.com>
2024-12-04 23:22:40 +0100

    (feat): mise à jour de la configuration Cypress et ajout de tests pour la fonctionnalité de recherche (wip)

--- Code/Front-End/cypress.config.js
+++ Code/Front-End/cypress.config.js
@@ -1,7 +1,8 @@
-const { defineConfig } = require('cypress')
+import { defineConfig } from 'cypress'
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:5173/',
+    supportFile: false,
   },
 })
---
+++ Code/Front-End/cypress/e2e/search.cy.js
@@ -0,0 +1,22 @@
+describe('Search Functionality', () => {
+  it('should display search results when a search term is entered', () => {
+    // Visiter la page d'accueil
+    cy.visit('/')
+
+    // Saisir un terme de recherche dans la barre de recherche
+    cy.get('input[placeholder="Rechercher un livre"]').type('comte')
+
+    // Cliquer sur le bouton de recherche
+    cy.get('button').contains('Rechercher').click()
+
+    // Vérifier que la page de résultats de recherche est affichée
+    cy.url().should('include', '/search?q=comte')
+
+    // Vérifier que les résultats de recherche contiennent le terme recherché
+    cy.get('h1').should('contain', 'Résultats de recherche pour "comte"')
+    //cy.get('li').should('have.length.greaterThan', 0)
+    //cy.get('li').each(($el) => {
+    //  cy.wrap($el).should('contain.text', 'comte')
+    //})
+  })
+})
